d34e33b5595835f883c70ed6ba1f96d77099cb62
Alexandru Voica <alexandru.voica16@gmail.com>
2018-04-11 20:01:30 +0300

    Added package.json

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const searchForm = document.querySelector('#search-form');
 const searchField = document.querySelector('#search-field');
 const resultsContainer = document.querySelector('.results-container');
 const submitButton = document.querySelector('#submit-btn');
-let keyword = '';
 
-searchForm.addEventListener('submit', function (event) {
-  event.preventDefault();
-  resultsContainer.innerHTML = '';
-  keyword = searchField.value;
-});
+console.log(searchForm);
+
+let keyword = '';
+window.onload = function () {
+  searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    resultsContainer.innerHTML = '';
+    keyword = searchField.value;
+    console.log(keyword);
+  });
+};
+
 
 // fetch data from APIs
+
+
